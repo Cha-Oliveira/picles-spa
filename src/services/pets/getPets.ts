6a3431f26eeee6752b4c3a0ed99d9e1888ea7e83
Cha-Oliveira <charlespiedade@gmail.com>
2024-04-18 22:23:49 -0300

    criação hooks - update pets - getPets - main

--- src/services/pets/getPets.ts
+++ src/services/pets/getPets.ts
@@ -1,8 +1,9 @@
-import { GetPetsResponse } from '../../interfaces/pet'
+import { GetPetsRequest, GetPetsResponse } from '../../interfaces/pet'
 import httpClient from '../api/httpClient'
 
-export async function getPets(params?: GetPetsResponse): Promise<GetPetsResponse>{
+export async function getPets(params?: GetPetsRequest): Promise<GetPetsResponse>{
     try {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
     const response = await httpClient.get('/pet', {params})
     return response.data
     } catch (error) {
